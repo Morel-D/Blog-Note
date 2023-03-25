237fb1fe6ce1d803c3a89b85d0956ea183d48c82
Morel-D <morelDen@gmail.com>
2023-03-25 13:59:44 +0100

    CRUD: Read Data

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +9,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+// Collection in firestore
+  final CollectionReference _notes =
+      FirebaseFirestore.instance.collection('notes');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,62 +32,94 @@ class _HomeState extends State<Home> {
         backgroundColor: Color.fromARGB(255, 249, 249, 249),
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 10, 30, 30),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-              child: TextFormField(
-                decoration: InputDecoration(
-                    fillColor: Color.fromARGB(255, 209, 209, 209),
-                    filled: true,
-                    // icon: Icon(Icons.search),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: Colors.transparent)),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: Colors.transparent)),
-                    hintText: "Search for a note"),
-              ),
-            ),
-            SizedBox(height: 25),
-            Card(
+      // StreamBuilder helps you to keep a percistence connection with firestore database
+      body: StreamBuilder(
+          stream: _notes.snapshots(),
+          builder: (context,
+              AsyncSnapshot<QuerySnapshot>
+                  streamSnapshot) /* streamSnapshot will have all the data available in the documents */ {
+            if (streamSnapshot.hasData) {
+              return SafeArea(
                 child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: CircleAvatar(
-                      radius: 25.0,
-                      backgroundColor: Color.fromARGB(255, 209, 209, 209),
-                      backgroundImage: AssetImage('assets/pen.png'),
-                    ),
-                    title: Text('CHRONOS'),
-                    subtitle: Text('This is the chronos enterpreise'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
+                  padding: const EdgeInsets.fromLTRB(30, 10, 30, 30),
+                  child: Column(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.fromLTRB(25, 0, 0, 0),
-                        child: Text(
-                          '17:48, 24 march 2023',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 215, 215, 215)),
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              fillColor: Color.fromARGB(255, 209, 209, 209),
+                              filled: true,
+                              // icon: Icon(Icons.search),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent)),
+                              hintText: "Search for a note"),
                         ),
                       ),
-                      SizedBox(width: 200),
-                      IconButton(onPressed: () {}, icon: Icon(Icons.delete))
+                      SizedBox(height: 25),
+                      ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: streamSnapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            final DocumentSnapshot documentSnapshot =
+                                streamSnapshot.data!.docs[index];
+
+                            return Card(
+                                child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                children: [
+                                  ListTile(
+                                    leading: CircleAvatar(
+                                      radius: 30.0,
+                                      backgroundColor:
+                                          Color.fromARGB(255, 209, 209, 209),
+                                      backgroundImage:
+                                          AssetImage('assets/pen.png'),
+                                    ),
+                                    title: Text(documentSnapshot['title']),
+                                    subtitle: Text(documentSnapshot['text']),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            25, 0, 0, 0),
+                                        child: Text(
+                                          '17:48, 24 march 2023',
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 215, 215, 215)),
+                                        ),
+                                      ),
+                                      SizedBox(width: 200),
+                                      IconButton(
+                                          onPressed: () {},
+                                          icon: Icon(Icons.delete))
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ));
+                          })
                     ],
-                  )
-                ],
-              ),
-            )),
-          ],
-        ),
-      ),
+                  ),
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
   }
 }
