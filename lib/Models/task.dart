0696c735674697a4b651e8ecedb0d22014220627
Morel-D <morelDen@gmail.com>
2023-04-06 14:34:27 +0100

    Create service & model class for task functionality

---
+++ lib/Models/task.dart
@@ -0,0 +1,7 @@
+class Task {
+  final String title;
+  final String list;
+  final DateTime date;
+
+  Task({required this.title, required this.list, required this.date});
+}
---
+++ lib/Services/noteTask.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:blog_notes/Models/task.dart';
+
+final CollectionReference _tasks =
+    FirebaseFirestore.instance.collection('tasks');
+
+class NoteTask {
+// CRUD operation
+
+// Create The task
+
+  Future<Task?> createTask(String title, String task, String date) async {
+    try {
+      Map<String, dynamic> data = <String, dynamic>{
+        "title": title,
+        "task": task,
+        "date": date
+      };
+      await _tasks.add(data);
+    } catch (e) {
+      print("Something is wrong : ${e.toString()}");
+    }
+  }
+
+// Update the task
+
+  Future<void> updateTask(String taskId, Map<String, dynamic> task) async {
+    try {
+      await _tasks.doc(taskId).update(task);
+    } catch (e) {
+      print('Something is wrong ${e.toString()}');
+    }
+  }
+
+// Delete the task
+
+  Future<void> deleteTask(String taskId) async {
+    try {
+      await _tasks.doc(taskId).delete();
+    } catch (e) {
+      print("Somthing wrong has occur ${e.toString()}");
+    }
+  }
+}
